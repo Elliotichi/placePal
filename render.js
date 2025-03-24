c2a6d65b00ee6032d6f8e472ea74bb07edbd60ee
sraaaaaaay <srbroch@proton.me>
2025-03-24 18:04:38 +0000

    Update gitignore

--- render.js
+++
@@ -1,7 +0,0 @@
-exports.renderLogin = function (res, options = { success: "", failure: "" }) {
-    return res.render("pages/loginpage", options);
-}
-
-exports.renderHomepage = function (res, options = { success: "", failure: "" }) {
-    return res.render("pages/index", options);
-}
